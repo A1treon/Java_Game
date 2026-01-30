781d7e4b57817c704c980e246f188c804827ccf4
A1treon <allenvoss407@gmail.com>
2026-01-30 16:57:01 -0600

    Initial files

---
+++ ControlConstants.java
@@ -0,0 +1,9 @@
+public class ControlConstants {
+    
+    public class TimeContstants {
+        public static final double FRAME_RATE = 60.0;
+        public static final double DELTA_TIME = 1.0 / FRAME_RATE;
+        public static final int MILLISECONDS_PER_FRAME = (int)(1000.0 / FRAME_RATE);
+    }
+
+}
