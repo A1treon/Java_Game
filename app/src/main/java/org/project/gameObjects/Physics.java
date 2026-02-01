61139591664cbd26a9fe210675cfc02dfcb37640
A1treon <allenvoss407@gmail.com>
2026-01-31 23:19:04 -0600

    Player falling through

--- app/src/main/java/org/project/gameObjects/Physics.java
+++ app/src/main/java/org/project/gameObjects/Physics.java
@@ -18,6 +18,10 @@ public class Physics {
     public static void setDeltaTime(double deltaTime) {
         Physics.deltaTime = deltaTime;
     }
+    
+    public static double getDeltaTime() {
+        return deltaTime;
+    }
 
     public void setGravityOn(boolean gravityOn) {
         this.gravityOn = gravityOn;
